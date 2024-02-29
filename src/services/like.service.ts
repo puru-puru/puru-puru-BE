378e5b165496c454ce578134ebfa33217ceba969
803571 <8omsut@naver.com>
2024-03-01 04:06:05 +0900

    Feat: 대댓글 기능추가

--- src/services/like.service.ts
+++ src/services/like.service.ts
@@ -4,8 +4,8 @@ import { Boards } from "../../models/Boards";
 import { LikeRepository } from "../repositories/like.repository";
 
 export class LikeService {
-likeRepository = new LikeRepository()
-    
+    likeRepository = new LikeRepository()
+
     likePost = async (userId: any, boardId: any) => {
         try {
             const addLike = await this.likeRepository.likePost(userId, boardId);
@@ -16,9 +16,9 @@ likeRepository = new LikeRepository()
         }
     }
 
-    
-     // 게시물에 좋아요 취소
-     removeLike = async (userId: any, boardId: any) => {
+
+    // 게시물에 좋아요 취소
+    removeLike = async (userId: any, boardId: any) => {
         try {
             const unLike = await this.likeRepository.removeLike(userId, boardId);
 
@@ -27,6 +27,6 @@ likeRepository = new LikeRepository()
             throw err;
         }
     }
-    
+
 
 }
